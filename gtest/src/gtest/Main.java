bedde376574a308f3208feed54c82e6a826a5764
Kyle Brown <kbrow164@msudenver.edu>
2021-02-20 17:19:14 -0700

    Added transparent style as well as maxed viewing size.\ Still need to make it movable with the mouse and add an exit button. After which it is time to considering writing event handles for input of username and password.

--- gtest/src/gtest/Main.java
+++ gtest/src/gtest/Main.java
@@ -4,15 +4,20 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Scene scene = new Scene(root);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.setScene(scene);
+        scene.setFill(Color.TRANSPARENT);
+        primaryStage.setMaximized(true);
         primaryStage.show();
     }
 
