2b61a36a697af67c460a238967c789ee538d943e
Kyle Brown <kbrow164@msudenver.edu>
2021-02-21 14:22:14 -0700

    The login screen can now be dragged across the screen. Still needs an exit button, and in the future main will need to be cleaned up.

--- gtest/src/gtest/Controller.java
+++ gtest/src/gtest/Controller.java
@@ -1,4 +1,14 @@
 package gtest;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
 public class Controller {
+
+
 }
--- gtest/src/gtest/Main.java
+++ gtest/src/gtest/Main.java
@@ -1,25 +1,58 @@
 package gtest;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class Main extends Application {
+    private double Xoff = 0;
+    private double Yoff = 0;
+
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(root, 750, 600);
         primaryStage.initStyle(StageStyle.TRANSPARENT);
-        primaryStage.setScene(scene);
         scene.setFill(Color.TRANSPARENT);
-        primaryStage.setMaximized(true);
+        primaryStage.setScene(scene);
+
+    root.setOnMousePressed(new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent event) {
+            Xoff = event.getSceneX();
+            Yoff = event.getSceneY();
+        }
+    });
+
+    root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent event) {
+            primaryStage.setX(event.getScreenX() - Xoff);
+            primaryStage.setY(event.getScreenY() - Yoff);
+        }
+    });
+
+
+
+
+
+
+
         primaryStage.show();
     }
+
+
+
+
+
+
 
 
     public static void main(String[] args) {
